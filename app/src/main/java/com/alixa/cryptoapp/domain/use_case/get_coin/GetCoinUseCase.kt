d17ba2f7dae6df01d9fcd788ca2be6c500e19ac8
ali rahimi <ali.rahimi@divar.ir>
2024-02-18 20:27:15 +0330

    Impl Usecases of get coins and coin

---
+++ app/src/main/java/com/alixa/cryptoapp/domain/use_case/get_coin/GetCoinUseCase.kt
@@ -0,0 +1,28 @@
+package com.alixa.cryptoapp.domain.use_case.get_coin
+
+import com.alixa.cryptoapp.common.Resource
+import com.alixa.cryptoapp.data.remote.dto.toCoinDetails
+import com.alixa.cryptoapp.domain.model.CoinDetail
+import com.alixa.cryptoapp.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinUseCase @Inject constructor(
+    private val repository: CoinRepository
+) {
+
+    operator fun invoke(coinId: String): Flow<Resource<CoinDetail>> = flow {
+        try {
+            emit(Resource.Loading())
+            val coin = repository.getCoinById(coinId).toCoinDetails()
+            emit(Resource.Success(data = coin))
+        } catch (e: HttpException) {
+            emit(Resource.Error(message = e.localizedMessage ?: "An Unexpected error occured!"))
+        } catch (e: IOException) {
+            emit(Resource.Error(message = "Check your internet Connection!"))
+        }
+    }
+}
---
+++ app/src/main/java/com/alixa/cryptoapp/domain/use_case/get_coins/GetCoinsUseCase.kt
@@ -0,0 +1,28 @@
+package com.alixa.cryptoapp.domain.use_case.get_coins
+
+import com.alixa.cryptoapp.common.Resource
+import com.alixa.cryptoapp.data.remote.dto.toCoin
+import com.alixa.cryptoapp.domain.model.Coin
+import com.alixa.cryptoapp.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinsUseCase @Inject constructor(
+    private val repository: CoinRepository
+) {
+
+    operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val coins = repository.getCoins().map { it.toCoin() }
+            emit(Resource.Success(data = coins))
+        } catch (e: HttpException) {
+            emit(Resource.Error(message = e.localizedMessage ?: "An Unexpected error occured!"))
+        } catch (e: IOException) {
+            emit(Resource.Error(message = "Check your internet Connection!"))
+        }
+    }
+}
